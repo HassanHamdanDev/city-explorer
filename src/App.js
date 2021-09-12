69841cddc4b5aa29692d9f5866f5319fc9d28b47
HassanHamdanDev <hassanhamdandev@gmail.com>
2021-09-12 17:48:13 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+//import axios from 'axios';
 
 class App extends Component {
   render() {
